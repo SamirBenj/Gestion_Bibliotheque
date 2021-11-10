935c68dfcb7bd2257bc9b0bf94b9268344133c49
SamirBenj <sciencecalc7@gmail.com>
2021-11-10 13:43:02 +0100

    new file

--- src/Bibliotheque.java
+++ src/Bibliotheque.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Bibliotheque {
         private ArrayList<Livre> livres;
-
+        private ArrayList<Dvd> dvd;
         public bibliotheque(){
             livres = new ArrayList<Livre>();
         }
