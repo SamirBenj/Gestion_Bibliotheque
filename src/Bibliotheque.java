cb0ed40611003a337b43b070fb03c8b9b39c03c9
SamirBenj <sciencecalc7@gmail.com>
2021-11-10 13:39:37 +0100

    Initial commit

---
+++ src/Bibliotheque.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Bibliotheque {
+        private ArrayList<Livre> livres;
+
+        public bibliotheque(){
+            livres = new ArrayList<Livre>();
+        }
+
+
+}
---
+++ src/Livre.java
@@ -0,0 +1,15 @@
+public class Livre {
+
+    private String Titre;
+    private String auteur;
+    private String genre;
+    private int publicationYear;
+
+
+    public Livre(String titre, String auteur, String genre, int publicationYear) {
+        Titre = titre;
+        this.auteur = auteur;
+        this.genre = genre;
+        this.publicationYear = publicationYear;
+    }
+}
