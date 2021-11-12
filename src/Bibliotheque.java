385a1eeaa695101145288dddc048981ad89be8dd
SamirBenj <sciencecalc7@gmail.com>
2021-11-12 11:04:34 +0100

    menu ajoutée

--- src/Bibliotheque.java
+++ src/Bibliotheque.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Bibliotheque {
         private ArrayList<Livre> livres;
         private ArrayList<Dvd> dvd;
         private ArrayList<Revue> revue;
-
+        private Scanner sc;
         public Bibliotheque(){
+
             livres = new ArrayList<Livre>();
             dvd =  new ArrayList<Dvd>();
             revue = new ArrayList<Revue>();
@@ -22,14 +24,20 @@ public class Bibliotheque {
         }
 
         public void chercherAuteur(String auteur){
+            sc = new Scanner(System.in);
+
             Livre l =null;
-            System.out.println("Salut");
+            System.out.println("Veuillez entrer le nom de l'auteur");
+            String auteurClavier = sc.nextLine();
             for (int i=0; i< livres.size(); i++){
-                System.out.println(livres.get(i).getAuteur().toString());
-                if(livres.get(i).getAuteur().equals(auteur.toString())){
-                    System.out.println("Trouvé");
+                if(livres.get(i).getAuteur().equals(auteurClavier)){
+                    System.out.println("Voici les information de l'auteur trouvé");
+                    System.out.println("Titre :" + livres.get(i).getTitre().toString());
+                    System.out.println("Auteur :" + livres.get(i).getAuteur().toString());
+                    System.out.println("Genre :"+ livres.get(i).getGenre().toString());
+                    System.out.println("Année dde publication :" + livres.get(i).getPublicationYear().toString());
                 }else {
-                    System.out.println("Pas trouvé");
+                    System.out.println("Votre n'a pas était retrouvé");
                 }
             }
         }
